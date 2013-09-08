90ce12ce267ee7441938db889087d5e2eee9419f
roger <roger@elvex.org.ar>
2013-09-08 06:52:01 -0300

    moved wsgi wrappers outside of __main__

--- plugins/main.py
+++ plugins/main.py
@@ -1,10 +1,10 @@
 from rugmi.plugins.routing import application
 
+for wrapper in wsgi_wrappers:
+    application = wrapper(application)
+
 if __name__ == '__main__':
     import sys
-    for wrapper in wsgi_wrappers:
-        application = wrapper(application)
-
     if len(sys.argv) == 2 and sys.argv[1] == "http":
         from wsgiref.simple_server import make_server
         srv = make_server('localhost', 8000, application)
